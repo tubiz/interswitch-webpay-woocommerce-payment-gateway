79a5227bd05aa0e7c908df57b0cd22e47d6ca35c
Tunbosun Ayinla <tayinla@gmail.com>
2014-07-15 12:05:05 +0100

    Initial Commit

---
+++ interswitch-webpay-woocommere-payment-gateway.php
@@ -0,0 +1,12 @@
+<?php
+/*
+	Plugin Name: Interswitch Webpay WooCommerce Payment Gateway
+	Plugin URI: http://bosun.me/interswitch-webpay-woocommerce-payment-gateway
+	Description: Interswitch Webpay Woocommerce Payment Gateway allows you to accept payment on your Woocommerce store via Verve Cards, Visa Cards and Mastercards.
+	Version: 1.0.0
+	Author: Tunbosun Ayinla
+	Author URI: http://bosun.me/
+	License:           GPL-2.0+
+ 	License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ 	GitHub Plugin URI: https://github.com/tubiz/interswitch-webpay-woocommerce-payment-gateway
+*/
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Interswitch Webpay Woocommerce Payment Gateway ===
+Contributors: tubiz
+Donate link: http://bosun.me/donate
+Tags: woocommerce, payment gateway, payment gateways, mastercard, visa cards, mastercards, interswitch, verve cards, tubiz plugins, verve, nigeria
+Requires at least: 3.5
+Tested up to: 3.9.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Interswitch Webpay Woocommerce Payment Gateway allows you to accept payment on your Woocommerce store via Verve Cards, Visa Cards and Mastercards.
+
+
